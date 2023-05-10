bb62b7fe2b16710fa2d3ea83bac0993b8b428fa4
lai2-01 <lai2-01@itismeucci.com>
2023-05-10 11:45:50 +0200

    inizio add

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 // Example of a data array that
 // you might receive from an API
@@ -9,25 +10,69 @@ const data = [
 ]
 
 function App() {
+  const [students, setStudents] = useState(data);
+  const [showForm, setShowForm] = useState(false);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+
+
+  function elimina(el: String){
+    setStudents(students.filter(obj=> obj.name !== el));
+  }
+
+  function aggiungi(){
+    
+  }
+
+  function show_set(){
+    if(showForm){
+      setShowForm(false);
+    }else{
+      setShowForm(true);
+      }
+    
+
+  }
+
 return (
+  <center>
 	<div className="App">
 	<table>
 		<tr>
 		<th>Name</th>
 		<th>Age</th>
 		<th>Gender</th>
+		<th>Pulsante</th>
 		</tr>
-		{data.map((val, key) => {
+		{students.map((val, key) => {
 		return (
 			<tr key={key}>
 			<td>{val.name}</td>
 			<td>{val.age}</td>
 			<td>{val.gender}</td>
+      <td><button onClick={() => elimina(val.name)}>Elimina</button></td>
 			</tr>
 		)
 		})}
 	</table>
+
+  <div><button onClick={()=> show_set()}> Inserisci</button></div>
+  {showForm &&
+<form action='post'>
+  <div><label>Name:</label><input type="text" id='name' required/></div>
+<div><label>Age:</label><input type="number" id='age' required/></div>
+<div><label>Gender:</label><select id='gender' required>
+  <option value="female">Female</option>
+  <option value="male">Male</option>
+  </select></div>
+
+
+<div><button>Salva</button></div>
+</form>
+  }
 	</div>
+  </center>
 );
 }
 
